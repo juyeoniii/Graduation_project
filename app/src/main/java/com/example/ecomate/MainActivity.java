21ba348b8ad018aff175b6f40c9dfa43da6e696e
Songjimin194 <63646470+Songjimin194@users.noreply.github.com>
2020-04-26 18:40:43 +0900

    login액티비티랑 register액티비티

--- app/src/main/java/com/example/ecomate/MainActivity.java
+++ app/src/main/java/com/example/ecomate/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.ecomate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);'
+        setContentView(R.layout.activity_main);
 
+    }
 
-    ddddd
-
-    d6rdtrdj'
+    public void onButton2Clicked(View v){
+        Intent myIntent = new Intent(getApplicationContext(),RegisterActivity.class);
+        startActivity(myIntent);
     }
 }
