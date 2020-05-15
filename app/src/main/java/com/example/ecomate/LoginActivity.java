d3ba77b92e900dc2d2bba923e7f389e17b5e2e83
juyeoniii <63646436+juyeoniii@users.noreply.github.com>
2020-05-16 04:09:02 +0900

    5-15주연

--- app/src/main/java/com/example/ecomate/LoginActivity.java
+++ app/src/main/java/com/example/ecomate/LoginActivity.java
@@ -1,203 +1,80 @@
 package com.example.ecomate;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DownloadManager;
-import android.app.Notification;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.app.AlertDialog;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.ecomate.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
-    String id, pw, pwck, email;
-
-    EditText mEditTextName;
-    EditText mEditTextId;
-    EditText mEditTextpw;
-    EditText mEditTextpwck;
-    EditText mEditTextemail;
-
-    final Context context = this;
-
+    private EditText et_id, et_pass;
+    private Button btn_login,btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mEditTextId = (EditText) findViewById(R.id.idwrite);
-        mEditTextpw = (EditText) findViewById(R.id.pwwrite);
-    }
+        et_id=findViewById(R.id.et_id);
+        et_pass=findViewById(R.id.et_pass);
+        btn_login=findViewById(R.id.btn_login);
+        btn_register=findViewById(R.id.btn_register);
 
-    public void lgbtn(View v)
-    {
-
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-/*        try{
-            id = mEditTextId.getText().toString();
-            pw = mEditTextpw.getText().toString();
-        }catch (NullPointerException e)
-        {
-            Log.e("err",e.getMessage());
-        }
-
-
-        loginDB lDB = new loginDB();
-        lDB.execute();
-
-    }
-
-    public void rgbtn(View view)
-    {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
-    }
-
-    public class loginDB extends AsyncTask<Void, Integer, Void> {
-
-        String data = "";
-
-
-        @Override
-        protected Void doInBackground(Void... unused) {
-
-
-            String param = "id=" + id + "&id=" + pw +"";
-            Log.e("POST",param);
-            try {
-
-                URL url = new URL(
-                        "http://192.168.0.2/insert3.php");
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                conn.setRequestMethod("POST");
-                conn.setDoInput(true);
-                conn.connect();
-
-
-                OutputStream outs = conn.getOutputStream();
-                outs.write(param.getBytes("UTF-8"));
-                outs.flush();
-                outs.close();
-
-
-                InputStream is = null;
-                BufferedReader in = null;
-
-                is = conn.getInputStream();
-                in = new BufferedReader(new InputStreamReader(is), 8 * 1024);
-                String line = null;
-                StringBuffer buff = new StringBuffer();
-                while ( ( line = in.readLine() ) != null )
-                {
-                    buff.append(line + "\n");
-                }
-                data = buff.toString().trim();
-
-
-                Log.e("RECV DATA",data);
-
-                if(data.equals("0"))
-                {
-                    Log.e("RESULT","성공적으로 처리되었습니다!");
-                }
-                else
-                {
-                    Log.e("RESULT","에러 발생! ERRCODE = " + data);
-                }
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+        btn_register.setOnClickListener(new View.OnClickListener() {//회원가입 버튼을 클릭시 수행
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
             }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String userID=et_id.getText().toString();
+                String userPass=et_pass.getText().toString();
 
-            return null;
-        }
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
-
-            if(data.equals("1"))
-            {
-                Log.e("RESULT","성공적으로 처리되었습니다!");
-                alertBuilder
-                        .setTitle("알림")
-                        .setMessage("성공적으로 등록되었습니다!")
-                        .setCancelable(true)
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Response.Listener<String> responseListener=new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jasonObject=new JSONObject(response);
+                            boolean success=jasonObject.getBoolean("success");
+                            if (success) {//회원등록 성공한 경우
+                                String userID = jasonObject.getString("userID");
+                                String userPass = jasonObject.getString("userPassword");
+                                Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("log", "User");
+                                intent.putExtra("userID", userID);
                                 startActivity(intent);
-                                finish();
                             }
-                        });
-                AlertDialog dialog = alertBuilder.create();
-                dialog.show();
-            }
-            else if(data.equals("0"))
-            {
-                Log.e("RESULT","비밀번호가 일치하지 않습니다.");
-                alertBuilder
-                        .setTitle("알림")
-                        .setMessage("비밀번호가 일치하지 않습니다.")
-                        .setCancelable(true)
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                //finish();
+
+
+                            else{//회원등록 실패한 경우
+                                Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                                return;
+
                             }
-                        });
-                AlertDialog dialog = alertBuilder.create();
-                dialog.show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest=new LoginRequest(userID,userPass,responseListener);
+                RequestQueue queue= Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
             }
-            else
-            {
-                Log.e("RESULT","에러 발생! ERRCODE = " + data);
-                alertBuilder
-                        .setTitle("알림")
-                        .setMessage("등록중 에러가 발생했습니다! errcode : "+ data)
-                        .setCancelable(true)
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                finish();
-                            }
-                        });
-                AlertDialog dialog = alertBuilder.create();
-                dialog.show();
-            }
-        }
-*/
+        });
     }
-
-
-
-
 }
--- app/src/main/java/com/example/ecomate/RegisterActivity.java
+++ app/src/main/java/com/example/ecomate/RegisterActivity.java
@@ -1,180 +1,132 @@
 package com.example.ecomate;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.ecomate.R;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 
-import java.io.BufferedReader;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText et_id, et_pass, et_name,et_passck,et_email;
+    private Button btn_register,validateButton;
+    private AlertDialog dialog;
+    private boolean validate=false;
 
-    private static String IP_ADDRESS = "10.0.2.2";
-    private static String TAG = "phptest";
-
-    private EditText mEditTextName;
-    private EditText mEditTextId;
-    private EditText mEditTextpw;
-    private EditText mEditTextpwck;
-    private EditText mEditTextemail;
-    private TextView mTextViewresult;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        mEditTextName = (EditText)findViewById(R.id.nameregister);
-        mEditTextId = (EditText)findViewById(R.id.idregister);
-        mEditTextpw = (EditText)findViewById(R.id.pwregister);
-        mEditTextpwck = (EditText)findViewById(R.id.pwcheck);
-        mEditTextemail = (EditText)findViewById(R.id.emailregister);
-        mTextViewresult = (TextView) findViewById(R.id.mainresult);
-
-
-        mTextViewresult.setMovementMethod(new ScrollingMovementMethod());
-
-
-        Button buttonInsert = (Button)findViewById(R.id.bt_Join);
-        buttonInsert.setOnClickListener(new View.OnClickListener() {
+        //아이디 값 찾아주기
+        et_id=findViewById(R.id.et_id);
+        et_pass=findViewById(R.id.et_pass);
+        et_name=findViewById(R.id.et_name);
+        et_passck=findViewById(R.id.et_passck);
+        et_email=findViewById(R.id.et_email);
+        validateButton=findViewById(R.id.validateButton);
+        validateButton.setOnClickListener(new View.OnClickListener() {//id중복체크
             @Override
-            public void onClick(View v) {
-
-                String name = mEditTextName.getText().toString();
-                String id = mEditTextId.getText().toString();
-                String pw = mEditTextpw.getText().toString();
-                String pwck = mEditTextpwck.getText().toString();
-                String email = mEditTextemail.getText().toString();
-
-                if(pw.equals(pwck))
+            public void onClick(View view) {
+                String userID=et_id.getText().toString();
+                if(validate)
                 {
-                    InsertData task = new InsertData();
-                    task.execute("http://" + IP_ADDRESS + "/insert2.php", name,id,pw,pwck,email);
+                    return;
                 }
-
-
-                mEditTextName.setText("");
-                mEditTextId.setText("");
-                mEditTextpw.setText("");
-                mEditTextpwck.setText("");
-                mEditTextemail.setText("");
+                if(userID.equals("")){
+                    AlertDialog.Builder builder=new AlertDialog.Builder( RegisterActivity.this );
+                    dialog=builder.setMessage("아이디는 빈 칸일 수 없습니다")
+                            .setPositiveButton("확인",null)
+                            .create();
+                    dialog.show();
+                    return;
+                }
+                Response.Listener<String> responseListener=new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse=new JSONObject(response);
+                            boolean success=jsonResponse.getBoolean("success");
+                            if(success){
+                                AlertDialog.Builder builder=new AlertDialog.Builder( RegisterActivity.this );
+                                dialog=builder.setMessage("사용할 수 있는 아이디입니다.")
+                                        .setPositiveButton("확인",null)
+                                        .create();
+                                dialog.show();
+                                et_id.setEnabled(false);
+                                validate=true;
+                                validateButton.setText("확인");
+                            }
+                            else{
+                                AlertDialog.Builder builder=new AlertDialog.Builder( RegisterActivity.this );
+                                dialog=builder.setMessage("사용할 수 없는 아이디입니다.")
+                                        .setNegativeButton("확인",null)
+                                        .create();
+                                dialog.show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ValidateRequest validateRequest=new ValidateRequest(userID,responseListener);
+                RequestQueue queue= Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(validateRequest);
 
             }
         });
 
-    }
 
-    public void onClick(View view)
-    {
-        finish();
-    }
+        btn_register=findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //editText에 입력되어있는 값을 get(가져온다)해온다
+                String userID=et_id.getText().toString();
+                final String userPass=et_pass.getText().toString();
+                String userName=et_name.getText().toString();
+                final String PassCk=et_passck.getText().toString();
+                String userEmail=et_email.getText().toString();
 
-
-
-    class InsertData extends AsyncTask<String, Void, String>{
-        ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = ProgressDialog.show(RegisterActivity.this,
-                    "Please Wait", null, true, true);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            progressDialog.dismiss();
-            mTextViewresult.setText(result);
-            Log.d(TAG, "POST response  - " + result);
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            String name = (String)params[1];
-            String id = (String)params[2];
-            String pw = (String)params[3];
-            String email = (String)params[5];
-
-            String serverURL = (String)params[0];
-            String postParameters = "name=" + name + "&id=" + id + "&pw=" + pw + "&email=" + email;
-
-            try {
-
-                URL url = new URL(serverURL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-
-                httpURLConnection.setReadTimeout(5000);
-                httpURLConnection.setConnectTimeout(5000);
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.connect();
-
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                outputStream.write(postParameters.getBytes("UTF-8"));
-                outputStream.flush();
-                outputStream.close();
-
-
-                int responseStatusCode = httpURLConnection.getResponseCode();
-                Log.d(TAG, "POST response code - " + responseStatusCode);
-
-                InputStream inputStream;
-                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
-                    inputStream = httpURLConnection.getInputStream();
-                }
-                else{
-                    inputStream = httpURLConnection.getErrorStream();
-                }
-
-
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-
-                while((line = bufferedReader.readLine()) != null){
-                    sb.append(line);
-                }
-
-
-                bufferedReader.close();
-
-
-                return sb.toString();
-
-
-            } catch (Exception e) {
-
-                Log.d(TAG, "InsertData: Error ", e);
-
-                return new String("Error: " + e.getMessage());
+                Response.Listener<String> responseListener=new Response.Listener<String>() {//volley
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jasonObject=new JSONObject(response);//Register2 php에 response
+                            boolean success=jasonObject.getBoolean("success");//Register2 php에 sucess
+                            if(userPass.equals(PassCk)) {
+                                if (success) {//회원등록 성공한 경우
+                                    Toast.makeText(getApplicationContext(), "회원 등록 성공", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                }
+                            }
+                            else{//회원등록 실패한 경우
+                                Toast.makeText(getApplicationContext(),"회원 등록 실패",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                //서버로 volley를 이용해서 요청을 함
+                RegisterRequest registerRequest=new RegisterRequest(userID, userPass, userName, userEmail, responseListener);
+                RequestQueue queue= Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
             }
-
-        }
+        });
     }
-
 
 }
